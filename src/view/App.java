b24a60e6f868ebb4702769c7a2cc7ec9e9670ad2
will <wyoung348@gmail.com>
2017-06-05 22:44:47 -0400

    Added the beginnings of a JavaFX GUI as well as a view package

---
+++ src/view/App.java
@@ -0,0 +1,42 @@
+/* App.java
+ *
+ * Entry point for the application.
+ */
+
+//package com.github.synthsgw.view;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+	private static final String SCENE_FILE = "view/mainScene.fxml";
+	private static final String WINDOW_TITLE = "Window Title";
+
+	public static void main(String [] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage) {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource(SCENE_FILE));
+
+		try {
+			Parent root = loader.load();
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+		} catch(IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
+		stage.setTitle(WINDOW_TITLE);
+		stage.show();
+	}
+}
+
