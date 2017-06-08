ee95cd01bb18d15aca38bbd7e06def4e40d68ae9
Jonathan Brownlee <jon@Jonathans-MacBook-Air.local>
2017-06-08 15:47:23 -0400

    Refactored synthgw to synthsgw and SynthGW to SynthsGW.

--- src/synthsgw/SynthsGW.java
+++ src/synthsgw/SynthsGW.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package synthgw;
+package synthsgw;
 
 import userInterface.UIRunnable;
 
 
-public class SynthGW {
+public class SynthsGW {
 
     /**
      * @param args the command line arguments
