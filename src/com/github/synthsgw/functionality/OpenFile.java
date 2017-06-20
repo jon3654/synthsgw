aacff83a1da4051b30a3735e440e5906a4058045
Jonathan Brownlee <jon@Jonathans-MacBook-Air.local>
2017-06-20 15:02:42 -0400

    Renamed i variable to numberOfOpenFiles. Commented code.

--- src/com/github/synthsgw/functionality/OpenFile.java
+++ src/com/github/synthsgw/functionality/OpenFile.java
@@ -22,14 +22,14 @@ public class OpenFile {
     private static MediaPlayer player;
     static File file[];
     static int MAX = 10;
-    static int i;
+    static int numberOfOpenFiles;
     
     // class constructor
     public OpenFile(String str){
         fileChooser = new FileChooser();
         fileExt = str;
         file = new File[10];
-        i = 0;
+        numberOfOpenFiles = 0;
     }
     
     public static void openPlayer(File inFile){
@@ -50,9 +50,10 @@ public class OpenFile {
         songName = openFile.getName();
         // checks if file is of the correct type
         if(checkExt(openFile.getName().substring(openFile.getName().lastIndexOf('.') + 1)) == 0){
-            file[i] = openFile;
+            file[numberOfOpenFiles] = openFile;
+            // intializes the player
             openPlayer(openFile);
-            i++;
+            numberOfOpenFiles++;
             return 0;
         }
         return -1;
@@ -73,31 +74,31 @@ public class OpenFile {
     
     public static File getFile(){
         // returns null if no file is open
-        if(i < 1) return null;
-        return file[i-1];
+        if(numberOfOpenFiles < 1) return null;
+        return file[numberOfOpenFiles-1];
     }
     
     public int close(){
-        return removeFile(i-1);
+        return removeFile(numberOfOpenFiles-1);
     }
     
     // removes a file from the array
     private int removeFile(int n){
         // returns -1 if no file is open
-        if(i < 1) return -1;
+        if(numberOfOpenFiles < 1) return -1;
         
-        for(int j = n; n < i-1; j++){
+        for(int j = n; n < numberOfOpenFiles-1; j++){
             file[j] = file[j+1];
         }
-        file[i] = null;
-        i--;
+        file[numberOfOpenFiles] = null;
+        numberOfOpenFiles--;
         return 0;
     }
 
     
     // plays loaded file
     public static void play(){
-        if(i < 1) 
+        if(numberOfOpenFiles < 1) 
             noFileOpen();
         else
             player.play();
@@ -105,12 +106,12 @@ public class OpenFile {
     }
     
     public static void pause(){
-        if(i < 1) noFileOpen();
+        if(numberOfOpenFiles < 1) noFileOpen();
         else player.pause();
     }
     
     public static void stop(){
-        if(i < 1) noFileOpen();
+        if(numberOfOpenFiles < 1) noFileOpen();
         else player.stop();
     }
     
